4c1ead3fb4ab0c6b2332c9b3901192ae3d3db77d
hramtsov <hramtsov.p@gmail.com>
2021-06-12 13:34:16 +0300

    update fix

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ docker rm $(docker ps -a -q)
 docker images
 docker rmi $(docker images -q)
 git pull
-docker build -t voron .
-docker run -it -p 5000:5000 voron
+docker build -t aircheckin .
+docker run -it -p 5000:5000 aircheckin
